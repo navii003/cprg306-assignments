ca14747a6d259ad4374bd1222416a01e1be15b87
navii003 <navindutharindrasamarakkodi.hettiarachchige@edu.sait.ca>
2025-01-28 02:43:30 -0700

    Completed Assignment
    Created new folders, functions and used Links

---
+++ app/week-2/student-info.js
@@ -0,0 +1,12 @@
+import Link from "next/link";
+
+export default function StudentInfo() {
+    return (
+        <div>
+            <h1>Navindu Samarakkodi</h1>
+            <p>
+                <Link href="https://github.com/navii003/cprg306-assignments">https://github.com</Link>
+            </p>
+        </div>
+    );
+}
